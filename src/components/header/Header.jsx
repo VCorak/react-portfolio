8c1b8582cfb4572976e23c8c11faabe5c844b5f3
Valentina Corak <valentinacorak@gmail.com>
2021-09-03 17:55:18 +0200

    Adding material icons, styling left side of header, contact part

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,5 @@
 import "./Header.scss"
+import {Person, Mail} from "@material-ui/icons"
 
 export default function Header() {
     return (
@@ -6,6 +7,14 @@ export default function Header() {
             <div className="wrapper">
                 <div className="left">
                   <a href="#intro" className="logo">V>.</a>
+                    <div className="itemContainer">
+                        <Person className="icon" />
+                        <span>+46 7710 764</span>
+                    </div>
+                    <div className="itemContainer">
+                        <Mail className="icon" />
+                        <span>valentinacorak@gmail.com</span>
+                    </div>
                 </div>
                 <div className="right">
                 </div>
