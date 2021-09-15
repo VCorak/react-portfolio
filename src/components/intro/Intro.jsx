f84cca118cd3cc5bceaac2a68e3db3fbd9cc4377
Valentina Corak <valentinacorak@gmail.com>
2021-09-15 17:33:31 +0200

    Adding images for intro part, scss, two containers

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,7 +3,12 @@ import "./Intro.scss"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
-Intro
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="assets/profile.svg" alt=""/>
+                </div>
+            </div>
+            <div className="right"></div>
         </div>
     )
 }
