7589429b683df78fa6efe6d96f8642ba60b96585
Valentina Corak <valentinacorak@gmail.com>
2021-09-16 17:03:17 +0200

    Still messing with image, tryed some scg shape, deleted...wasted time...

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,21 +3,27 @@ import "./Intro.scss"
 export default function Intro() {
     return (
         <div className="intro" id="intro">
+
+
             <div className="left">
+                <div className="hoverContainer"></div>
                 <div className="imgContainer">
                     <img src="assets/profile2.png" alt=""/>
                 </div>
+
             </div>
+
+
             <div className="right">
                 <div className="wrapper">
                     <h2>Hi There, I'm</h2>
                     <h1>Valentina Corak</h1>
                     <h3>Junior<span></span></h3>
                 </div>
-                <a href="#portfolio">
+                <a href="#projects">
                 <img src="assets/down.png" alt="down arrow" />
                 </a>
             </div>
         </div>
-    )
-}
+        );
+            }
