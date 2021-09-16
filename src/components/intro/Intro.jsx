8f2e5079d6e053324c6764fdf37e0dfb2af0e98b
Valentina Corak <valentinacorak@gmail.com>
2021-09-16 13:57:12 +0200

    Added profile picture on left side of intro section, have to work on it still...starting right side of intro

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -5,10 +5,19 @@ export default function Intro() {
         <div className="intro" id="intro">
             <div className="left">
                 <div className="imgContainer">
-                    <img src="assets/profile.svg" alt=""/>
+                    <img src="assets/profile2.png" alt=""/>
                 </div>
             </div>
-            <div className="right"></div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Valentina Corak</h1>
+                    <h3>Junior<span></span></h3>
+                </div>
+                <a href="#portfolio">
+                <img src="assets/down.png" alt="down arrow" />
+                </a>
+            </div>
         </div>
     )
 }
