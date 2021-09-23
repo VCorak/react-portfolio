ccf30716494e8cec0f4d2180e3257a2eb3fa64cd
Valentina Corak <valentinacorak@gmail.com>
2021-09-23 18:17:36 +0200

    About me paragraph fixed and hope gaian the mobile scroll..

--- src/components/aboutme/AboutMe.jsx
+++ src/components/aboutme/AboutMe.jsx
@@ -15,13 +15,13 @@ export default function AboutMe() {
                     <div className="about">
                     <h2>About Me</h2>
                     <p> Hello! My name is Valentina and I enjoy creating things that live on the internet. My interest in web development started back in 2015 when I
-                        decided to try editing first Hello world page — turns out hacking together a custom divs and h1s taught me a lot about HTML & CSS! But life had another plans for me and...
-                        <span>Fast-forward to today,</span> my path started in online course SheCodes where I wrote my first lines of code in JavaScript and React.
-                        Realizing I have to learn more and get some internship opportunities I jumped into BeCode bootcamp.
+                        decided to try editing first Hello world page — turns out hacking together a custom divs and h1s taught me a lot about HTML & CSS!
+                        <span> Fast-forward to today,</span> my path started in one online course where I wrote my first lines of code in JavaScript and React.
+                        Realizing I have to learn more I jumped into BeCode bootcamp.
                         And here I am, writing my first portfolio in React, have 20+ projects and exercises behind me and group of people who were there for me when it was "I can never center a div, I give up" moment.
                         <span> Love running, woods and baking cakes. Chocolate cakes. </span>
                         </p>
-                </div>
+                    </div>
                 </div>
                 <a href="#projects">
                     <img src="assets/down.png" alt="down arrow" />
